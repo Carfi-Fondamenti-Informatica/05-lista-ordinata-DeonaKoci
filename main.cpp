ff1c8ba70afebd7786b518938c2a9762d5ddca08
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-05-02 00:47:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+ int lista[10];
+ for(int i=0;i<10;i++){
+ cin >> lista[i];
+ }
+ int a=0;
+ cin >> a;
+
+
+if ( a > lista[9]) {
+    for(int i=0; i<10; i++){
+    cout << lista[i] << " ";
+}}
+else {
+
+for(int i=0;i<10;i++){
+int j=lista[i];
+
+    for ( ; lista [i]<a;i++){
+        cout << lista[i] << " ";
+
+
+    }
+    for ( ; j<=a;j++){
+        j=a;
+        cout << j << " ";
+        break;
+    }
+    for ( ; j>a; j++ ){
+        cout << lista [i-1]<< " ";
+        break;
+    }
+}
+}
+
+ return 0;
 }
